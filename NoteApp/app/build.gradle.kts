29cac1aa8d52d3dbb9eb44eb11723f9cea78561c
alexandra-marin29 <alexandra-a.marin@student.unitbv.ro>
2024-12-15 01:10:55 +0200

    Implemented fragments

--- NoteApp/app/build.gradle.kts
+++ NoteApp/app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.noteapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.noteapp"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
