b16912235bcc410c93b928d4f829e7b72b3487aa
alexandra-marin29 <alexandra-a.marin@student.unitbv.ro>
2024-12-15 02:22:40 +0200

    Bug fixed show added notes

--- NoteApp/app/src/main/java/com/example/noteapp/fragments/HomeFragment.kt
+++ NoteApp/app/src/main/java/com/example/noteapp/fragments/HomeFragment.kt
@@ -54,21 +54,18 @@ class HomeFragment : Fragment(R.layout.fragment_home), SearchView.OnQueryTextLis
     }
 
     private fun updateUI(note: List<Note>?){
-        if(note!=null)
-        {
+        if(note != null){
             if(note.isNotEmpty()){
                 binding.emptyNotesImage.visibility = View.GONE
-                binding.homeRecyclerView.visibility = View.GONE
-
+                binding.homeRecyclerView.visibility = View.VISIBLE
             }
-            else
-            {
+            else{
                 binding.emptyNotesImage.visibility = View.VISIBLE
                 binding.homeRecyclerView.visibility = View.GONE
-
             }
         }
     }
+
 
     private fun setupHomeRecyclerView(){
         noteAdapter = NoteAdapter()
